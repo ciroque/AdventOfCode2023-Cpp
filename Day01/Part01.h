6ffbfcca7feb54d4345ad9923172511b324ac230
steve wagner <ciroque@live.com>
2023-12-01 16:40:00 -0800

    - Solves Day 1, Part 2 - Root interface for Puzzle Solutions - Extract common load logic to base class

--- Day01/Part01.h
+++ Day01/Part01.h
@@ -11,20 +11,15 @@
 
 namespace Day01 {
     class Part01 : public Day01::Day01 {
-    private:
-        static std::vector<std::string> RemoveCharactersFromPuzzleInput(const std::vector<std::string>& puzzleData);
-        static std::string RemoveCharactersFromLine(const std::string& line);
-        static std::vector<int> ResolveCalibrationValues(const std::vector<std::string>& puzzleDataOnlyNumbers);
-        static std::string ResolveSingleDigitCalibrationValue(const std::string& calibrationValue);
-        static std::string ResolveMultiDigitCalibrationValue(const std::string& calibrationValue);
 
-        static const int CALIBRATION_VALUE_INSERT_INDEX = 0;
-        static const int CALIBRATION_VALUE_EXTRACT_INDEX = 0;
-        static const int CALIBRATION_VALUE_INSERT_LENGTH = 2;
     public:
         std::string Solve(const std::string& puzzleInputFilename) override;
 
-        static int SumCalibrationValues(const std::vector<int>& calibrationValues);
+        static const int CALIBRATION_VALUE_EXTRACT_INDEX = 0;
+        static const int CALIBRATION_VALUE_INSERT_INDEX = 0;
+        static const int CALIBRATION_VALUE_INSERT_LENGTH = 2;
+
+        static std::string RemoveCharactersFromLine(const std::string& line);
     };
 }
 
